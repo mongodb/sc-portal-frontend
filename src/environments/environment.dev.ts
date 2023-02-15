93f95513f715b15dddd65742fd6e66f52ab5a6a1
Josh Smith <josh.smith@mongodb.com>
2023-02-15 09:47:31 -0600

    Aligned URL's to make the dev environment work

--- src/environments/environment.dev.ts
+++ src/environments/environment.dev.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  app_url: 'http://sc-portal.sc-north.staging.corp.mongodb.com/api'
+  app_url: 'http://sc-portal.sc-north.staging.corp.mongodb.com/api/'
 };
--- src/environments/environment.local.ts
+++ src/environments/environment.local.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  app_url: 'http://localhost:8080'
+  app_url: 'http://localhost:8080/api/'
 };
